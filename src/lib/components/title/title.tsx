e63aed2a176c032561b42ca06a9729e4c4fc694d
RAJDEEP-2003 <111244394+RAJDEEP-2003@users.noreply.github.com>
2024-12-27 11:55:32 +0530

    first commit

---
+++ src/lib/components/title/title.tsx
@@ -0,0 +1,21 @@
+import { Home } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import classes from './title.module.scss';
+
+interface Props {
+  title: string;
+}
+
+function Title({ title }: Props) {
+  return (
+    <h1 className={classes.title} data-testid="title">
+      <Link to="/" className={classes.home}>
+        <Home size={24} />
+      </Link>
+      {title}
+     
+    </h1>
+  );
+}
+
+export default Title;
